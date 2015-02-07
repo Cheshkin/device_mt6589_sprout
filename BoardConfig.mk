6980221de35e364b5d831e0be65db09d1a92c426
cheshkin <che16rus@gmail.com>
2015-02-08 00:18:13 +0300

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,87 @@
+LOCAL_PATH := device/google/sprout
+#LOCAL_PATH_CONFIG := kernel/mediatek/sprout/arch/arm/configs
+TARGET_BOARD_PLATFORM := mt6589
+CONFIG_MTK_PLATFORM := mt6589
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_VFP := true
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+ARCH_ARM_HAVE_NEON := true
+TARGET_NO_BOOTLOADER := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_CPU_VARIANT:= cortex-a7
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00B00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 11534336
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_KERNEL_CMDLINE :=
+#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
+#BOARD_KERNEL_BASE := 0x80000000
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/boot.mk
+BOARD_KERNEL_PAGESIZE := 2048
+DEVICE_RESOLUTION := 720x1280
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/google/sprout/bluetooth
+
+
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/root/fstab.sprout
+
+# kernel
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+
+TARGET_KERNEL_SOURCE := kernel/mediatek/google/sprout
+TARGET_KERNEL_CONFIG := che_cm11_mt6589_defconfig
+
+BOARD_RECOVERY_SWIPE := true
+KERNEL_TOOLCHAIN := /home/che/android_tools/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin
+# TWRP stuff
+
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+#TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+
+USE_OPENGL_RENDERER:= true
+
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_WLAN_DEVICE := mediatek
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_STA:=P2P
+
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_USERIMAGES_USE_EXT4:=true
+USE_CAMERA_STUB := true
+
+BOARD_RIL_CLASS := ../../../device/google/sprout/ril/
+
+BOARD_SEPOLICY_DIRS += \
+    device/google/sprout/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    device.te \
+    netd.te
